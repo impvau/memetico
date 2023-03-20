cdd5d6c8a05e5dec3ed6992ce437a21f0967b6f5
Andrew Ciezak <andy@impv.au>
2023-03-20 19:22:20 +1100

    integrate data.h into data_set files and mimimise

---
+++ memetico/data/data_set.cpp
@@ -0,0 +1,224 @@
+
+/**
+ * @file
+ * @author Andrew Ciezak <andy@impv.au>
+ * @version 1.0
+ * @brief Implementation of the DataSet class
+*/
+
+#include <memetico/data/data_set.h>
+
+vector<string> DataSet::IVS;
+
+void DataSet::load() {
+
+    // Ensure we can open file
+    //cout << "Loading " << filename << endl;
+    ifstream f;
+    f.open(filename);
+    if (!f.is_open())
+        throw runtime_error("Unable to open file "+ filename);
+
+    // Load data
+    bool is_first = true;
+    string line;
+    while ( f >> line ) { 
+
+        if(is_first)    load_header(line);
+        else            load_data(line);
+
+        is_first = false;
+    }
+
+    if( get_gpu() )
+        setup_gpu();
+}
+
+void DataSet::load_header(string line) {
+    
+    stringstream ss(line);
+    string part;
+
+    DataSet::IVS.clear();
+
+    size_t column = 0;
+    while(ss.good()) {
+
+        getline(ss, part, ',');    
+        
+        // Trim and remove line carrage if created from different os
+        string word = trim(part);
+        size_t index = word.find("\r", 0);
+        if (index != string::npos)
+            word.replace(index, 1, ""); 
+
+        // Process element between commans
+        if( word.compare("w") == 0 )            // If weight header
+            weight_column = column;
+        else if( word.compare("dy") == 0 )      // If uncertainty header
+            uncertainty_column  = column;
+        else if( word.compare("y") == 0 )       // If target header
+            target_column = column;     
+        else                                    // else its a variable
+            DataSet::IVS.push_back(word);    
+
+        column++;
+        
+    }    
+}
+
+void DataSet::load_data(string line ) {
+
+    stringstream ss(line);
+    string part;
+
+    size_t column = 0;
+    vector<double> vars;
+    while(ss.good()) {
+
+        getline(ss, part, ',');    
+        
+        // Trim and remove line carrage if created from different os
+        string word = trim(part);
+        size_t index = word.find("\r", 0);
+        if (index != string::npos)
+            word.replace(index, 1, ""); 
+
+        // Process element between commans
+        if( column == weight_column )          
+            weight.push_back(stod(word));
+        else if( column == uncertainty_column )     
+            dy.push_back(stod(word));
+        else if( column == target_column )      
+            y.push_back(stod(word));
+        else           
+            vars.push_back(stod(word));
+
+        column++;
+        
+    } 
+    samples.push_back(vars);
+
+}
+
+vector<size_t> DataSet::subset(float pct, bool to_GPU) {
+
+    size_t ret_count = (long) (pct * get_count());
+    vector<size_t> ret = RandInt::RANDINT->unique_set(ret_count, 0, get_count());
+
+    if( gpu ) {
+
+        // Free subset if already exists
+        if(device_data.subset_size > 0) {
+            freeSubset(&device_data);
+            device_data.subset_size = 0;
+        }
+
+        // Copy new subset
+        copySubset(&device_data, ret);
+    }
+
+    return ret;
+
+}
+
+/**
+ * Output DataSet state
+ * 
+ * @param   out         Output stream to write to
+ *                      Defaults to cout
+ * 
+ * @param   precision   resolution of real numerical output
+ *                      Defaults to meme::PREC
+ * 
+ * @return          void
+ */
+/*
+void DataSet::show(ostream& out, size_t precision) {
+
+    size_t temp_precision = out.precision();
+    out.precision(precision);
+
+    size_t pad = precision+8;
+    
+    for(size_t i = 0; i < count; i++) {
+
+        // Header
+        if( i == 0) {
+                
+            out << setw(8) << "#" << setw(8) << "Sample" << setw(pad) << "y";
+            for(size_t j = 0; j < ivs; j++)
+                out << setw(pad) << names[j];
+            
+            if(has_uncertainty())
+                out << setw(pad) << "dy";
+
+            if(has_weight())
+                out << setw(pad) << "weight";
+
+            out << endl;
+            
+        }
+
+        out << setw(8) << i+1;
+        out << setw(8) << number[i];
+        out << setw(pad) << y[i];
+
+        for(size_t j = 0; j < ivs; j++)                    
+            out << setw(pad) << variables[i][j];
+
+        if(has_uncertainty())
+            out << setw(pad) << dy[i];
+
+        if(has_weight())
+            out << setw(pad) << weight[i];
+            
+        out << endl;
+    }
+
+    cout << endl;
+    out.precision(temp_precision);
+    
+}
+*/
+
+void DataSet::csv(string file_name) {
+
+    ofstream f;
+    f.open(file_name);
+    if (!f.is_open())
+        throw runtime_error("Unable to open file "+ file_name);
+    
+    for(size_t i = 0; i < get_count(); i++) {
+
+        // Header
+        if( i == 0) {
+                
+            f << "y";
+            for(size_t j = 0; j < DataSet::IVS.size(); j++)
+                f << "," << DataSet::IVS[j];
+            
+            if(has_uncertainty())
+                f << "," << "dy";
+
+            if(has_weight())
+                f << "," << "weight";
+
+            f << endl;
+        }
+
+        f << y[i];
+
+        for(size_t j = 0; j < DataSet::IVS.size(); j++)                    
+            f << ","  << samples[i][j];
+
+        if(has_uncertainty())
+            f << ","  << dy[i];
+
+        if(has_weight())
+            f << ","  << weight[i];
+            
+        f << endl;
+    }  
+}
+
---
+++ memetico/data/data_set.h
@@ -0,0 +1,142 @@
+
+/**
+ * @file
+ * @author Andrew Ciezak <andy@impv.au>
+ * @version 1.0
+ * @brief Header for the DataSet class
+*/
+
+#ifndef MEMETICO_DATA_SET_H
+#define MEMETICO_DATA_SET_H
+// Std
+#include <cstdlib>
+#include <iomanip>
+#include <fstream>
+#include <memetico/gpu/gpu_dataset.h>
+#include <memetico/gpu/cuda.cuh>
+#include <memetico/helpers/rng.h>
+#include <memetico/helpers/text.h>
+
+using namespace cusr;
+
+/**
+ * Class representing a set of data samples
+ */
+class DataSet {
+
+    public:
+
+        /** Construct DataSet with file */
+        DataSet(string file_name, bool do_gpu = false) { 
+            filename = file_name; 
+            gpu = do_gpu;
+            target_column = -1;
+            weight_column = -1;
+            uncertainty_column = -1;
+        };
+
+        /** @brief Free GPU data */
+        ~DataSet() {
+            if( gpu ) {
+                if(device_data.subset_size > 0) {
+                    freeSubset(&device_data);
+                    device_data.subset_size = 0;
+                }
+
+                freeDataSetAndLabel(&device_data);
+            }
+        }
+
+        /** Target values for samples */
+        vector<double>          y;
+
+        /** Independent variable values for samples */
+        vector<vector<double>>  samples;
+
+        /** Weight values for samples */
+        vector<double>          weight;
+
+        /** Uncertainty values for samples  */
+        vector<double>          dy;
+
+        /** Names of the independent varaibles */
+        static vector<string>   IVS;
+
+        /** @brief Return filename for DataSet */
+        string get_file()       { return filename; };
+
+        /** @brief Return number of samples */
+        size_t get_count()      { return y.size(); };
+
+        /** @brief Return if GPU is being used */
+        bool get_gpu()          {return gpu;};
+
+        /** @brief Return if all samples have weight */
+        bool has_weight()       { return weight.size() == get_count();}
+
+        /** @brief Return if all samples have uncertainty */
+        bool has_uncertainty()  { return dy.size() == get_count();}
+
+        /** @brief Load data from filename into the object */
+        void load();
+
+        /** @brief Output DataSet to CSV */
+        void csv(string file_name);
+
+        /** @brief Get indexes for a percentage of the DataSet uniformly and at random */
+        vector<size_t> subset(float pct, bool to_GPU = true);
+
+        // GPU specific 
+
+        /** @brief GPU dataset for use in cuda.cuh */
+        GPUDataset device_data;
+
+        /** @brief GPU initialisation */
+        void setup_gpu() {
+
+            // GPU must be configured 
+            if(!gpu)    return;
+
+            vector<vector<float>> float_samples;
+            for(size_t i = 0; i < samples.size(); i++) {
+                vector<float> temp;
+                for(size_t j = 0; j < IVS.size(); j++) {
+                    temp.push_back(samples[i][j]);
+                }
+                float_samples.push_back(temp);
+            }
+
+            vector<float> float_y;
+            for(size_t i = 0; i < y.size(); i++)
+                float_y.push_back(y[i]);
+
+            copyDatasetAndLabel(&device_data, float_samples, float_y); 
+            device_data.subset_size = 0;
+        }
+    
+    private: 
+
+        /** Load first row of file */
+        void load_header(string s);
+
+        /** Load data line in file */
+        void load_data(string s);
+
+        /** Location of file used to generate dataset */
+        string          filename;
+
+        /** Weight column */
+        int             weight_column;
+
+        /** Uncertainty column */
+        int             uncertainty_column;
+
+        /** Target column */
+        int             target_column;
+
+        /** Flag for GPU operation */
+        bool            gpu;
+
+};
+
+#endif
