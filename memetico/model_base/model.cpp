5b1a6212afb928dc6e2da554ba35a9fc15c87f98
Andrew Ciezak <andy@impv.au>
2023-06-08 12:13:54 +1000

    remove reference to objective func

--- memetico/model_base/model.cpp
+++ memetico/model_base/model.cpp
@@ -2,8 +2,3 @@
 #include <memetico/model_base/model.h>
 
 PrintType Model::FORMAT = PrintExcel;
-
-string Model::OBJECTIVE_NAME = "";
-
-double (*Model::OBJECTIVE)(Model*, DataSet*, vector<size_t>&) = nullptr;
-
