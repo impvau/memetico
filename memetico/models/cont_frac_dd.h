4cfa1350f4245821f52a4877523dd5e6949d94b4
Andrew Ciezak <andy@impv.au>
2023-06-08 12:16:15 +1000

    use global depth to facilitate bcf

--- memetico/models/cont_frac_dd.h
+++ memetico/models/cont_frac_dd.h
@@ -3,7 +3,6 @@
  * @author Andrew Ciezak <andy@impv.au>
  * @version 1.0
  * @brief ContinuedFractionDynamicDepth is a ContinuedFraction that can dynamically change depth
- * @copyright (C) 2022 Prof. Pablo Moscato, License CC-BY
  */
 
 #ifndef MEMETICO_MODELS_CONT_FRAC_DD_H_
@@ -29,45 +28,42 @@ enum DynamicDepthType {
 /**
  * @brief The ContinuedFractionAdaptiveDepth class extends the ContinuedFraction class with adaptive depth logic
  */
-template<class T>
-class ContinuedFractionDynamicDepth : public ContinuedFraction<T> {
+template<typename T, typename U>
+class ContinuedFractionDynamicDepth : public ContinuedFraction<T,U> {
 
     public:
 
         /**
-         * @brief Construct ContinuedFractionAdaptiveDepth as per parent ContinuedFraction<T>,
+         * @brief Construct ContinuedFractionAdaptiveDepth as per parent ContinuedFraction<T,U>,
          * however using a depth in the range [best_model_depth-1, best_model_depth+1] while ensuring 
          * depth is not negative (reset to 0 when attempted)
          */
-        ContinuedFractionDynamicDepth() : ContinuedFraction<T>( determine_depth() ) {};
+        ContinuedFractionDynamicDepth(size_t depth = ContinuedFractionDynamicDepth<T,U>::determine_depth()) : ContinuedFraction<T,U>( depth ) {};
 
-        ContinuedFractionDynamicDepth(const ContinuedFraction<T> &o) :
-            ContinuedFraction<T>::ContinuedFraction<T>(o) {
-            
-        };
+        /** @brief Copy constructor */
+        ContinuedFractionDynamicDepth(const ContinuedFractionDynamicDepth<T,U> &o) :
+            ContinuedFraction<T,U>::ContinuedFraction<T,U>(o) {};
 
-        void    mutate(MemeticModel<T> & model) override {
+        /** @brief Mutate operator 
+         * Mutate as standard ContinuedFraction but if DynamicAdaptiveMutation is set, then re-determine depth
+        */
+        void    mutate(MemeticModel<U> & model) override {
             
             // Call normal mutate
-            ContinuedFraction<T>::mutate(model);
+            ContinuedFraction<T,U>::mutate(model);
 
             // If we are using the adaptive mutate approach
             if( DYNAMIC_DEPTH_TYPE == DynamicAdaptiveMutation ) {
 
                 size_t d = determine_depth();
-                //cout << "AdaptiveMutate depth: " << this->get_depth() << " new_depth: " << d << endl;
                 this->set_depth(d);
             }
         };
 
-        static DynamicDepthType DYNAMIC_DEPTH_TYPE;
-
-    private:
-    
         /** @brief return a depth uniformly at random between [meme::POCKET_DEPTH-1, meme::POCKET_DEPTH+1], setting to 0 when negative */ 
-        size_t determine_depth() {
+        static size_t determine_depth() {
 
-            int rand = (int)ContinuedFraction<T>::DEPTH;
+            int rand = meme::DEPTH;
 
             // If adaptive approach or adaptive mutatution approach
             if(DYNAMIC_DEPTH_TYPE == DynamicAdaptive || DYNAMIC_DEPTH_TYPE == DynamicAdaptiveMutation) {
@@ -79,15 +75,20 @@ class ContinuedFractionDynamicDepth : public ContinuedFraction<T> {
 
             // If random depth
             } else if (DYNAMIC_DEPTH_TYPE == DynamicRandom)
-                rand = meme::RANDINT(0, ContinuedFraction<T>::DEPTH);
+                rand = meme::RANDINT(0, 5);
             
-            //cout << "Creating Fraction Depth: " << rand << " Global_Depth: " << meme::POCKET_DEPTH << endl;
+            //cout << "New fraction of depth: " << rand << endl;
 
             return size_t(rand);
         };
+
+        static DynamicDepthType DYNAMIC_DEPTH_TYPE;
+
+    private: 
+        
 };
 
-template<class T>
-DynamicDepthType ContinuedFractionDynamicDepth<T>::DYNAMIC_DEPTH_TYPE = DynamicNone;
+template<typename T, typename U>
+DynamicDepthType ContinuedFractionDynamicDepth<T,U>::DYNAMIC_DEPTH_TYPE = DynamicNone;
     
 #endif
