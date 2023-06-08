74608bf8488905441af628d38959531281e5a5ac
Andrew Ciezak <andy@impv.au>
2023-06-08 12:15:40 +1000

    add logic for bcf

---
+++ memetico/models/branch_cont_frac_dd.h
@@ -0,0 +1,201 @@
+
+/** @file
+ * @author Andrew Ciezak <andy@impv.au>
+ * @version 1.0
+ * @brief ContinuedFraction is MemeticModel where each fraction term is a Regression
+ */
+
+#ifndef MEMETICO_MODELS_BRANCH_CONT_FRAC_H_
+#define MEMETICO_MODELS_BRANCH_CONT_FRAC_H_
+
+// Local
+#include <memetico/helpers/safe_ops.h>
+#include <memetico/model_base/model_meme.h>
+#include <memetico/helpers/print.h>
+#include <memetico/models/regression.h>
+#include <memetico/models/cont_frac_dd.h>
+
+// Std
+#include <stdexcept>
+#include <typeinfo>
+#include <iostream>
+#include <sstream>
+#include <math.h>
+
+/**
+ * @brief 
+ */
+template<typename T, typename U>
+class BranchedContinuedFraction : public ContinuedFractionDynamicDepth<T,U> {
+
+    public:
+
+        BranchedContinuedFraction(size_t not_used = 0) : ContinuedFractionDynamicDepth<T,U>(this->determine_depth()) {
+        //BranchedContinuedFraction(size_t not_used = 0) : ContinuedFractionDynamicDepth<T,U>(1) {
+
+            this->sub_depth = meme::DEPTH;
+
+            for(size_t i = 0; i < this->get_frac_terms(); i++) {
+                T temp = T(this->sub_depth);
+                this->set_terms(i, temp);
+                if(i == 0 )
+                    this->set_params_per_term(this->get_terms(0).get_param_count());
+            }
+
+        };
+
+        BranchedContinuedFraction(const BranchedContinuedFraction<T,U> &o) : ContinuedFractionDynamicDepth<T,U>(o) {
+            
+            this->sub_depth = o.sub_depth;
+
+            for(size_t i = 0; i < this->get_frac_terms(); i++) {
+                T temp = T(this->terms[i]);
+                this->set_terms(i, temp);
+                if(i == 0 )
+                    this->set_params_per_term(this->get_terms(0).get_param_count());
+            }
+            
+        };
+
+        size_t  get_param_count() { 
+            size_t params = 0;
+            for(size_t i = 0; i < this->get_frac_terms(); i++) {
+                params += this->get_terms(i).get_param_count();
+            }
+            return params;
+        };
+
+        /** @brief Overwrite as 0, does not make sense as each term of the fraction has different global active */
+        bool    get_global_active(size_t iv) const  { return 0; };
+
+        size_t  get_count_active() { 
+            size_t count = 0;
+            for(size_t i = 0; i < this->get_frac_terms(); i++) {
+                count += this->get_terms(i).get_count_active();
+            }
+            return count;
+        };
+
+        void    mutate(MemeticModel<U>& pocket) override {
+
+            return;
+
+            // We shift depth, but maintain the same depth across all terms
+            size_t new_depth = this->determine_depth();
+
+            // Mutate each term
+            for(size_t i = 0; i < this->get_frac_terms(); i++) {
+                this->get_terms(i).mutate(pocket);
+                this->get_terms(i).set_depth(new_depth);
+            }
+
+        };
+
+        /**  
+         * @brief recombine \a this MemeticModel considering two other MemeticModels
+         * - Determine the recombination method to apply to all T fraction terms
+         * - For all terms in the fraction
+         *  - Call the underlying recombination method in T
+         *  - Force the use of the global recombination method
+         * 
+         * @param m1 first MemeticModel
+         * @param m2 second MemeticModel
+         */
+        void    recombine(MemeticModel<U>* model1, MemeticModel<U>* model2, int method_override = -1) override {
+
+            BranchedContinuedFraction<T,U>* m1 = static_cast<BranchedContinuedFraction<T,U>*>(model1);
+            BranchedContinuedFraction<T,U>* m2 = static_cast<BranchedContinuedFraction<T,U>*>(model2);
+
+            int method = RandInt::RANDINT->rand(0,2);
+
+            //cout << "m1: " << *m1 << endl << endl;
+            //cout << "m2: " << *m2 << endl << endl;
+
+            // Minimum from m1/m2
+            size_t min_terms = min(m1->get_frac_terms(), m2->get_frac_terms());
+
+            // Minimum of the above with the current object
+            min_terms = min(min_terms, this->get_frac_terms());
+
+            for(size_t i = 0; i < min_terms; i++) {
+
+                //cout << "m1 t" << i << ": " << m1->get_terms(i) << endl << endl;
+                //cout << "m2 t" << i << ": " << m1->get_terms(i) << endl << endl;
+                //cout << m1->get_terms(i) << endl;
+                //cout << m2->get_terms(i) << endl;
+
+                this->get_terms(i).recombine( &(m1->get_terms(i)), &(m2->get_terms(i)), method);
+            }
+
+        }
+
+        vector<size_t>  get_active_positions() {
+            
+            vector<size_t> active_pos;
+            for(size_t i = 0; i < this->get_frac_terms(); i++) {
+
+                vector<size_t> temp_pos = this->get_terms(i).get_active_positions();
+                for (auto& pos : temp_pos) {
+                    pos += this->get_terms(i).get_param_count()*i;
+                }
+
+                active_pos.insert(active_pos.end(), temp_pos.begin(), temp_pos.end());
+
+            }
+
+            return active_pos;
+        }
+
+        bool operator== (ContinuedFraction<T,U>& o) {
+
+            if( this->get_depth() != o.get_depth())
+                return false;
+
+            if( this->get_frac_terms() != o.get_frac_terms() )
+                return false;
+
+            for(size_t i = 0 ; i < this->get_frac_terms(); i++) {
+                if( !(this->get_terms(i) == o.get_terms(i)) )
+                    return false;
+            }
+
+            // Else we match!
+            return true;
+        }
+
+        void set_depth(size_t new_depth) {
+
+            size_t  new_frac_terms = 2*new_depth+1;
+
+            // If we are reducing terms, simply remove the number of terms from the vector
+            if( new_frac_terms < this->get_frac_terms()) {
+
+                size_t excess_terms = this->get_frac_terms()-new_frac_terms;
+                for(size_t i = 0; i < excess_terms; i++)
+                    this->pop_terms();
+
+            // If we are adding terms, we need to increase terms
+            } else {
+
+                size_t new_terms = new_frac_terms-this->get_frac_terms();
+                for(size_t i = 0; i < new_terms; i++) {
+                    T new_term  = T(1);
+                    new_term.randomise();
+                    this->add_terms(new_term);
+                }
+
+            }
+
+            this->set_depth_val(new_depth);
+
+        }
+
+        size_t get_sub_depth() {return this->sub_depth;};
+
+    private:
+
+        size_t sub_depth;
+
+};
+
+#endif
---
+++ memetico/models/branch_cont_frac_dd.test.cpp
@@ -0,0 +1,295 @@
+
+#include "doctest.h"
+#include <memetico/models/regression.h>
+#include <memetico/models/cont_frac.h>
+#include <memetico/models/branch_cont_frac_dd.h>
+#include <sstream>
+#include <memetico/optimise/objective.h>
+#include <vector>
+
+typedef double DataType;
+typedef ContinuedFraction<Regression<DataType>,DataType> TermType;
+typedef BranchedContinuedFraction<TermType,DataType> ModelType;
+
+inline void setup_cont_frac_ivs(size_t size) {
+
+    // Setup IVs in Regression
+    ModelType::IVS.clear();
+    for(size_t i = 0; i < size-1; i++)
+        ModelType::IVS.push_back("x"+to_string(i+1));
+
+}
+
+inline Regression<double> build_reg_frac(vector<bool> active, vector<double> vals, size_t params) {
+
+    if( active.size() != params )
+        throw invalid_argument("active.size() != params");
+
+    if( vals.size() != params )
+        throw invalid_argument("vals.size() != params");
+
+    Regression<double> o = Regression<double>(params);
+    for(size_t i = 0; i < params; i++) {
+        o.set_active(i, active[i]);
+        o.set_value(i, vals[i]);
+    }
+
+    return o;
+}
+
+inline TermType build_cont_frac(vector<bool> active, vector<double> vals, vector<bool> globals, size_t params, size_t depth) {
+
+    if( globals.size() != params)
+        throw invalid_argument("globals.size() != params");
+
+    if( active.size() != params*(2*depth+1) )
+        throw invalid_argument("active.size() != params*2*depth");
+
+    if( vals.size() != params*(2*depth+1) )
+        throw invalid_argument("vals.size() != params*2*depth");
+
+    TermType c = TermType(depth);
+
+    for(size_t i = 0; i < params; i++)
+        c.set_global_active(i, globals[i]);
+
+    size_t terms = 2*depth+1;
+    for(size_t i = 0; i < terms; i++) {
+
+        vector reg_active(active.begin()+i*params, active.begin()+i*params+params);
+        vector reg_vals(vals.begin()+i*params, vals.begin()+i*params+params);
+        Regression<double> o = build_reg_frac(reg_active,reg_vals,params);
+        c.set_terms(i, o);
+    }
+
+    return c;
+}
+
+inline TermType frac_4() {
+
+    // t1(x) =  x1 + 2x3 + 3x5 - 20
+    // t2(x) = -3x2 + 4x4 + 3x5 - 3
+    // t3(x) =  2x1 + x3 + x5 - 5
+    // f(x) = t1(x) + t2(x) / t3(x)
+    return build_cont_frac(
+        {
+            true, false, true, false, true, true,
+            false, true, false, true, true, true,
+            true, false, true, false, true, true
+        },
+        {
+            1, 12, 2, -7, 3, -20,
+            0, -3, 0, 4, 3, -3,
+            2, 12, 1, -7, 1, -5
+        },
+        {   true, true, true, true, true, true },
+        6,
+        1
+    );
+}
+
+/*
+TEST_CASE("BranchedContinuedFractions: constructor, set_depth, get_terms, get_frac_terms, set_value, get_value") {
+
+    RandInt ri = RandInt(42);
+    RandReal rr = RandReal(42);
+    RandInt::RANDINT = &ri;
+    RandReal::RANDREAL = &rr;
+
+    size_t params = 2;
+    setup_cont_frac_ivs(params);
+
+    // Test depth of 0 fraction
+    meme::DEPTH = 0;
+    ModelType o1(4);
+    REQUIRE(o1.get_depth() == 0);
+    TermType t = o1.get_terms(0);
+    REQUIRE(o1.get_sub_depth() == 4);
+    REQUIRE(t.get_depth() == 4);
+    stringstream ss;
+    ss << t;
+    // Hard coded results from random seed 42
+    string s = "(11*B2+19)+(-26*B2+22)/((-11*B2+1)+(-29*B2-1)/((22*B2+25)+(-2*B2-24)/((B2+6)+(30*B2+6)/((-12*B2-21)))))";
+    REQUIRE(ss.str() == s);
+    // Cont Frac
+    ss.str("");
+    ss << o1;
+    REQUIRE(ss.str() == "("+s+")");
+    REQUIRE(o1.get_frac_terms() == 1);
+    REQUIRE(o1.get_terms(0) == t);
+    REQUIRE(o1.get_param_count() == t.get_param_count());
+
+    // Test depth of 2 fractions
+    ri = RandInt(42);
+    rr = RandReal(42);
+    RandInt::RANDINT = &ri;
+    RandReal::RANDREAL = &rr;
+    meme::DEPTH = 1;
+    ModelType o2(4);
+    // Check Term 1 (strings based on initial seed = 42)
+    TermType t1 = o2.get_terms(0);
+    REQUIRE(t1.get_depth() == 4);
+    ss.str("");
+    ss << t1;
+    string s1 = "(24*B2+19)+(6*B2+12)/((16*B2+30)+(18*B2+15)/((-29*B2-5)+(-12*B2-10)/((-25*B2-17)+(-29*B2+1)/((29*B2+29)))))";
+    REQUIRE(ss.str() == s1);
+    // Check Term 2
+    TermType t2 = o2.get_terms(1);
+    REQUIRE(t2.get_depth() == 4);
+    ss.str("");
+    ss << t2;
+    string s2 = "(-2)+(17)/((-6)+(12)/((-21)+(-30)/((-28)+(7)/((21)))))";
+    REQUIRE(ss.str() == s2);
+    // Check Term 3
+    TermType t3 = o2.get_terms(2);
+    REQUIRE(t3.get_depth() == 4);
+    ss.str("");
+    ss << t3;
+    string s3 = "(8*B2+14)+(21*B2-27)/((19*B2-16)+(-6*B2+2)/((-25*B2-3)+(12*B2-23)/((-25*B2-11)+(-8*B2-21)/((13*B2+16)))))";
+    REQUIRE(ss.str() == s3);
+    // Check Fraction
+    ss.str("");
+    ss << o2;
+    string s4 = "("+s1+")+("+s2+")/(("+s3+"))";
+    REQUIRE(o2.get_frac_terms() == 3);
+    REQUIRE(o2.get_terms(0) == t1);
+    REQUIRE(o2.get_terms(1) == t2);
+    REQUIRE(o2.get_terms(2) == t3);
+    REQUIRE(o2.get_param_count() == t1.get_param_count()+t2.get_param_count()+t3.get_param_count());
+    REQUIRE(ss.str() == s4);
+
+    // Check set/get values
+    for(size_t i = 0; i < o2.get_param_count(); i++ )
+        o2.set_value(i, i*1.0);
+
+    // Check updated values - note that if the active is set to off, then get_value(i) = 0.0
+    for(size_t i = 0; i < o2.get_param_count(); i++ )
+        REQUIRE( (o2.get_value(i) == i*1.0 || o2.get_value(i) == 0.0) );
+
+    // Check set/get active
+    for(size_t i = 0; i < o2.get_param_count(); i++ ) {
+        if( i % 2 == 0 )
+            o2.set_active(i, true);
+        else
+            o2.set_active(i, false);
+    }
+    for(size_t i = 0; i < o2.get_param_count(); i++ ) {
+        if( i % 2 == 0 )
+            REQUIRE(o2.get_active(i) == true);
+        else
+            REQUIRE(o2.get_active(i) == false);
+    }
+    for(size_t i = 0; i < o2.get_param_count(); i++ ) {
+        if( i % 2 == 0 )
+            o2.set_active(i, false);
+        else
+            o2.set_active(i, true);
+    }
+    for(size_t i = 0; i < o2.get_param_count(); i++ ) {
+        if( i % 2 == 0 )
+            REQUIRE(o2.get_active(i) == false);
+        else
+            REQUIRE(o2.get_active(i) == true);
+    }
+
+}
+
+
+TEST_CASE("BranchedContinuedFractions: evaluate ") {
+
+    RandInt ri = RandInt(42);
+    RandReal rr = RandReal(42);
+    RandInt::RANDINT = &ri;
+    RandReal::RANDREAL = &rr;
+
+    size_t params = 2;
+    setup_cont_frac_ivs(params);
+
+    meme::DEPTH = 1;
+    ModelType o2(4);
+    // ((24*B2+19)+(6*B2+12)/((16*B2+30)+(18*B2+15)/((-29*B2-5)+(-12*B2-10)/((-25*B2-17)+(-29*B2+1)/((29*B2+29))))))+((-2)+(17)/((-6)+(12)/((-21)+(-30)/((-28)+(7)/((21))))))/(((8*B2+14)+(21*B2-27)/((19*B2-16)+(-6*B2+2)/((-25*B2-3)+(12*B2-23)/((-25*B2-11)+(-8*B2-21)/((13*B2+16)))))))
+
+    vector<double> vals = {4};
+    REQUIRE( abs(115.2884968-o2.evaluate(vals)) < 0.000001 );
+    
+    vals = {20.15};
+    REQUIRE( abs(502.9518641-o2.evaluate(vals)) < 0.000001 );
+    
+    vals = {-4.315};
+    REQUIRE( abs(-83.97205100-o2.evaluate(vals)) < 0.000001 );
+
+    // Maple code
+    // f := B2 -> 24*B2 + 19 + (6*B2 + 12)/(16*B2 + 30 + (18*B2 + 15)/(-29*B2 - 5 + (-12*B2 - 10)/(-25*B2 - 17 + (-29*B2 + 1)/(29*B2 + 29)))) - 979/214*1/(8*B2 + 14 + (21*B2 - 27)/(19*B2 - 16 + (-6*B2 + 2)/(-25*B2 - 3 + (12*B2 - 23)/(-25*B2 - 11 + (-8*B2 - 21)/(13*B2 + 16)))));
+}
+
+TEST_CASE("BranchedContinuedFractions: BranchedContinuedFractions(BranchedContinuedFractions o), ==") {
+
+    RandInt ri = RandInt(42);
+    RandReal rr = RandReal(42);
+    RandInt::RANDINT = &ri;
+    RandReal::RANDREAL = &rr;
+
+    Model::FORMAT = PrintLatex;
+
+    size_t params = 2;
+    setup_cont_frac_ivs(params);
+
+    meme::DEPTH = 1;
+    ModelType o2(4);
+    
+    ModelType o3 = ModelType(o2);
+    REQUIRE(o2 == o3);
+
+}
+*/
+
+/*TEST_CASE("ContinuedFractions: mutate") {
+  
+    RandInt ri = RandInt(42);
+    RandReal rr = RandReal(42);
+    RandInt::RANDINT = &ri;
+    RandReal::RANDREAL = &rr;
+
+    size_t params = 6;
+    size_t depth = 1;
+    setup_cont_frac_ivs(params);
+
+    ModelType o;
+    o.set_depth(1);
+    o.mutate(o);
+
+}*/
+
+/*TEST_CASE("ContinuedFractions: recombine") {
+  
+    RandInt ri = RandInt(42);
+    RandReal rr = RandReal(42);
+    RandInt::RANDINT = &ri;
+    RandReal::RANDREAL = &rr;
+
+    size_t params = 6;
+    size_t depth = 1;
+    setup_cont_frac_ivs(params);
+
+    ModelType o;
+    o.set_depth(1);
+    o.mutate(o);
+
+}*/
+
+/*TEST_CASE("ContinuedFractions: get node") {
+  
+    RandInt ri = RandInt(42);
+    RandReal rr = RandReal(42);
+    RandInt::RANDINT = &ri;
+    RandReal::RANDREAL = &rr;
+
+    size_t params = 6;
+    size_t depth = 1;
+    setup_cont_frac_ivs(params);
+
+    ModelType o;
+    o.set_depth(1);
+    o.mutate(o);
+
+}*/
